6363a4199112d91fee7c8e4ee46b5e87aff6b118
Al Mizan <67356360+almizanshimul@users.noreply.github.com>
2025-05-05 09:53:52 +0600

    header fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,45 +23,49 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="navbar shadow-sm bg-[#1D232A]  text-white ">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="  btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="shadow-sm bg-[#1D232A]  text-white">
+      <div className="navbar w-[1080px] mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              {menu}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            {menu}
-          </ul>
+          <Link className="flex items-center">
+            <img
+              src={AdvancedITLogo}
+              alt="Advanced IT Logo"
+              className="h-8 sm:h-12 md:h-14 lg:h-16 xl:h-18 object-contain brightness-0 invert-[1]"
+              width="180px"
+              height="50px"
+            />
+          </Link>
         </div>
-        <Link className="flex items-center">
-          <img
-            src={AdvancedITLogo}
-            alt="Advanced IT Logo"
-            className="h-8 sm:h-12 md:h-14 lg:h-16 xl:h-18 w-auto object-contain"
-          />
-        </Link>
+        <div className="navbar-center hidden lg:flex ">
+          <ul className="menu menu-horizontal px-1 ">{menu}</ul>
+        </div>
+        <div className="navbar-end">{<a className="btn bg-[#5A38C2]">Contact</a>}</div>
       </div>
-      <div className="navbar-center hidden lg:flex ">
-        <ul className="menu menu-horizontal px-1 ">{menu}</ul>
-      </div>
-      <div className="navbar-end">{/* <a className="btn">Button</a> */}</div>
     </div>
   );
 };
